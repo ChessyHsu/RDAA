d01f6975cb2aca77183c33495e24ff633891c491
ChessyHsu <chessy2206@gmail.com>
2017-02-07 09:27:17 +0800

    amusement park

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-for(var i = 1; i <= 27; i++) {
-  var duration = 1 + Math.random() * 3;
-  $("#star" + i).css({
-    "animation" : duration + "s gleam infinite ease"
-  })
-}
+//Disclaimer for FF users (I am one of them)
+
+// For correct animation please view this SVG through webkit browsers. Firefox is known for issue with transform-origin property specifically in svg. Nothing works well until you'll include transform-box: fill-box; (works in FF only) in CSS AND enable svg-transform-box property in about:config file. Thanks, Mozilla
